32016615b5f6fbcba78df1e80f7255914042af01
PEPSoares <psoares@edigma.com>
2023-03-03 16:14:02 +0000

    Start extruding proto

--- Assets/Edigma/Scripts/MorphedCylinder.cs
+++ Assets/Edigma/Scripts/MorphedCylinder.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering;
 public class MorphedCylinder : MonoBehaviour
 {
+    public bool extrude = false;
     public bool autoRotate = false;
     public float rotFactor = 1.0f;
     public float morphForce = 1.0f;
@@ -31,7 +32,7 @@ public class MorphedCylinder : MonoBehaviour
     float[] cos;
     float[] sin;
 
-    bool debug = false;
+    public bool debug = false;
     void DoTriangles()
     {
         for (int i = 0; i < (layers + (layers / 3)) - 1; i++)
@@ -46,7 +47,6 @@ public class MorphedCylinder : MonoBehaviour
                 triangles[triangleIndex] = i1;
                 triangles[triangleIndex + 1] = i3;
                 triangles[triangleIndex + 2] = i2;
-
                 triangles[triangleIndex + 3] = i2;
                 triangles[triangleIndex + 4] = i3;
                 triangles[triangleIndex + 5] = i4;
@@ -70,6 +70,7 @@ public class MorphedCylinder : MonoBehaviour
     void GenerateInit()
     {
         mesh = new Mesh();
+        mesh.indexFormat = IndexFormat.UInt32;
         GetComponent<MeshFilter>().mesh = mesh;
 
         int numVertices = (segments + 1) * (layers + (layers / 3));
@@ -117,17 +118,13 @@ public class MorphedCylinder : MonoBehaviour
                 normals[vertexIndex] = (i > layers ? -1 : 1) * new Vector3(x, 0f, z).normalized;
                 verticesRadius[vertexIndex] = thick_radius;
 
-                if (false)
+                if (debug)
                 {
                     GameObject oo = Instantiate(vert, new Vector3(0, 0, 0), Quaternion.identity);
                     oo.transform.parent = this.transform;
                     oo.transform.localPosition = vertices[vertexIndex];
-                    oo.name = " " + vertexIndex;
-                    if (i == 15 && j == 0)
-                    {
-                        //Debug.Log("created" + vertexIndex + " " + oo.transform.localPosition);
-                    }
-                    //verts[vertexIndex] = oo;
+                    oo.name = " " + vertexIndex + " " + j + " " + i;
+                    verts[vertexIndex] = oo;
                 }
             }
         }
@@ -136,46 +133,11 @@ public class MorphedCylinder : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("-.s" + startMesh.vertices.Length);
-
         GenerateInit();
         DoTriangles();
-
-        /*
-                mesh = new Mesh();
-
-                GetComponent<MeshFilter>().mesh = mesh;
-                vertices = (Vector3[])startMesh.vertices.Clone();
-                normals = (Vector3[])startMesh.normals.Clone();
-                triangles = (int[])startMesh.triangles.Clone();
-                transform.localScale = Vector3.one * 10;
-                transform.localPosition = Vector3.zero;
-                transform.localEulerAngles = new Vector3(-90.0f, 0, 0);
-                verticesRadius = new float[vertices.Length];
-                int i = 0;
-                foreach (Vector3 vertI in vertices)
-                {
-
-                    Vector3 yVWorldPos = transform.TransformPoint(vertI);
-                    float rad = Mathf.Atan2(yVWorldPos.y, yVWorldPos.x);
-                    verticesRadius[i] = rad;
-                    i++;
-
-                    //if (i < 500 || i > 19000)
-                    //{
-                    //    GameObject oo = Instantiate( i < 500 ? vert2 : vert, new Vector3(0, 0, 0), Quaternion.identity);
-                    //    oo.transform.parent = this.transform;
-                    //    oo.transform.localPosition = vertices[i];
-                    //    oo.name = i + " o " + rad;
-                    //}
-                }
-        */
         Apply();
-
         MeshRenderer renderer = GetComponent<MeshRenderer>();
         renderer.material = meshMaterial;
-
-
     }
 
     public float anim = 1.0f;
@@ -229,7 +191,7 @@ public class MorphedCylinder : MonoBehaviour
 
             Vector3 pos = go.transform.position;
 
-            for (int i = 0; i < (layers + (layers / 3) - 1); i++)
+            for (int i = 0; i < (layers + (layers / 3)); i++)
             {
                 float y = 0;
                 if (i > layers)
@@ -252,7 +214,7 @@ public class MorphedCylinder : MonoBehaviour
                 }
 
 
-                for (int j = 0; j <= segments; j++)
+                for (int j = 0; j <= (segments); j++)
                 {
                     int vertexIndex = i * segments + j;
                     Vector3 vertPos = vertices[vertexIndex];
@@ -270,26 +232,53 @@ public class MorphedCylinder : MonoBehaviour
                     {
                         Vector3 vertWorldPos = transform.TransformPoint(vertPos);
                         float force = Vector3.Magnitude(vertWorldPos - pos);
-
+/*
                         if (force < go.interactionRadius)
                         {
-                            float realForce = (go.interactionRadius - force);
-
-                            vradius -= 0.1f * Time.deltaTime * morphForce * realForce;
+                            if (dist > vradius)
+                            {
+                                vradius -= 0.05f * Time.deltaTime;
+                            }
+                            else
+                            {
+                                vradius -= 0.05f * Time.deltaTime;
+                            }
                         }
 
+*/
+
+                        if (extrude)
+                        {
+                            if (force < vradius)
+                            {
+                                float realForce = (go.interactionRadius - force);
+
+                                vradius += 0.1f * Time.deltaTime * morphForce * realForce;
+                            }
+                        }
+                        else
+                        {
+                            if (force < go.interactionRadius)
+                            {
+                                float realForce = (go.interactionRadius - force);
+                                vradius -= 0.1f * Time.deltaTime * morphForce * realForce;
+                            }
+                        }
+                        
 
                         if (vradius <= radius * 0.1f)
                         {
                             vradius = radius * 0.1f;
                         }
 
+                        if (vradius >= radius * 1.1f)
+                        {
+                            vradius = radius * 1.1f;
+                        }
                     }
 
                     float x = cos[j] * vradius;
                     float z = sin[j] * vradius;
-
-
 
                     verticesRadius[vertexIndex] = vradius;
                     vertices[vertexIndex] = new Vector3(x, vertPos.y, z);
